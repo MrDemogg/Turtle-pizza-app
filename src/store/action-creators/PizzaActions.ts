5c3488aca9dbf9b32132b4aeefa9469fc79af4c2
demog <dg08gg@mail.ru>
2022-10-13 19:01:00 +0300

    Данные в реальном времени
    Для получения данных в реальном времени, переработал код под модуль firebase. Это и задумовалось изначально, но не получалось, теперь получилось

--- src/store/action-creators/PizzaActions.ts
+++ src/store/action-creators/PizzaActions.ts
@@ -1,26 +1,32 @@
 import {Dispatch} from "redux";
 import {PizzaActions, PizzaActionTypes} from "../../types/pizza";
+import {initializeApp} from 'firebase/app';
 import {getDatabase, onValue, ref} from "firebase/database";
-import {initializeApp} from "firebase/app";
 
 export const FetchDishes = () => {
   return async (dispatch: Dispatch<PizzaActions>) => {
     try {
       dispatch({type: PizzaActionTypes.FETCH_DISHES})
       const firebaseConfig = {
-        databaseURL: 'https://turtle-pizza-69d09-default-rtdb.europe-west1.firebasedatabase.app/'
-      }
-      const app = initializeApp(firebaseConfig)
-      const database = getDatabase(app)
+        apiKey: "AIzaSyDM3wwBqFN2W2kKSou8n_hN5__eNxF70yE",
+        authDomain: "turtle-pizza-69d09.firebaseapp.com",
+        databaseURL: "https://turtle-pizza-69d09-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "turtle-pizza-69d09",
+        storageBucket: "turtle-pizza-69d09.appspot.com",
+        messagingSenderId: "1084253055868",
+        appId: "1:1084253055868:web:de9385e5f212bc628f52f9",
+        measurementId: "G-4X9G12CP1H"
+      };
+      let myApp = initializeApp(firebaseConfig);
       const db = getDatabase();
       const starCountRef = ref(db, 'dishes');
       onValue(starCountRef, (snapshot) => {
-        const data = snapshot.val()
+        const data = snapshot.val();
         dispatch({
           type: PizzaActionTypes.FETCH_SUCCESS_DISHES,
           payload: data
         })
-      })
+      });
     } catch (e) {
       dispatch({
         type: PizzaActionTypes.FETCH_ERROR_DISHES,
