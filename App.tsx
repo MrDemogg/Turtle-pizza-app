2fd0f2488bbfc9f2d973f4bc4b0022b178cecedc
demog <dg08gg@mail.ru>
2022-10-14 14:04:42 +0300

    Соединение
    Для отображение компонентов, ипортировал в основной файл App.tsx

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import {Provider} from "react-redux";
 import {store} from "./src/store";
+import Dishes from "./src/components/Dishes";
 
 export default function App() {
   return (
     <Provider store={store}>
       <View>
+        <Dishes/>
         <StatusBar style="auto" />
       </View>
     </Provider>
