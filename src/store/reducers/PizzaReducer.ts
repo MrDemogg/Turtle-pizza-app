63ddd9217a50a499a54ce895d6c923c7f1e182a4
demog <dg08gg@mail.ru>
2022-10-14 14:46:44 +0300

    Хранилище
    Как раз-таки новые данные хранилища и способы их обработки, прописанные в редюсере

--- src/store/reducers/PizzaReducer.ts
+++ src/store/reducers/PizzaReducer.ts
@@ -4,7 +4,10 @@ const initialState: PizzaState = {
   dishes: null,
   error: null,
   loading: false,
-  order: null
+  order: {
+    cart: [],
+    totalPrice: 150
+  }
 }
 
 export const PizzaReducer = (state = initialState, action: PizzaActions): PizzaState => {
@@ -15,8 +18,35 @@ export const PizzaReducer = (state = initialState, action: PizzaActions): PizzaS
       return {...state, loading: false, error: action.payload}
     case PizzaActionTypes.FETCH_SUCCESS_DISHES:
       return {...state, loading: false, dishes: action.payload}
-    case PizzaActionTypes.SET_ORDER:
-      return {...state, order: action.payload}
+    case PizzaActionTypes.ADD_ORDER_CART:
+      const newCart = [...state.order.cart]
+      for (let i = 0; i < newCart.length; i++) {
+        if (newCart[i].title === action.payload) {
+          const initialPrice = newCart[i].price / newCart[i].amount
+          newCart[i].amount += 1
+          newCart[i].price += initialPrice
+          return {...state, order: {...state.order, cart: newCart}}
+        }
+      }
+      newCart.push(action.payload)
+      return {...state, order: {...state.order, cart: newCart}}
+    case PizzaActionTypes.ADD_ORDER_PRICE:
+      const totalPrice = state.order.totalPrice + action.payload
+      return {...state, order: {...state.order, totalPrice: totalPrice}}
+    case PizzaActionTypes.REMOVE_ORDER_DISH:
+      const selectedDish = state.order.cart.filter((dish: any) => dish.title === action.payload)
+      const CartWithoutDish = state.order.cart.filter((dish: any) => dish.title !== action.payload)
+      if (selectedDish[0].amount > 1) {
+        let index = 0
+        for (let i = 0; i < state.order.cart.length; i++) {
+          if (state.order.cart[i].title === action.payload) {
+            index += i
+          }
+        }
+        const newSelectedDish = {...state.order.cart[index], amount: state.order.cart[index].amount--}
+        return {...state, order: {...state.order, cart: [...CartWithoutDish, newSelectedDish]}}
+      }
+      return {...state, order: {...state.order, cart: CartWithoutDish}}
     default:
       return state
   }
